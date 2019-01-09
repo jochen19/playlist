4f62f43f1ced45b98c185c7f08e4ef42793cc751
Jonathan Chen <jonathanbaby88@gmail.com>
2019-01-09 02:06:32 +0000

    Added images and links

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@
 // BELOW Update the songs to your favorites songs. There should be at least 4.
 var songs = [
     "Rewind Youtube Style 2012",
-    "Youtube Rewing: What Does 2013 Say?",
+    "Youtube Rewind: What Does 2013 Say?",
     "Youtube Rewind: Turn Down for 2014",
     "Youtube Rewind: Now Watch Me 2015",
     "Youtube Rewind: The Ultimate 2016 Challenge",
@@ -23,7 +23,13 @@ var images_links = [
 ];
 
 var artists = [
-    "Youtube"
+    "Youtube",
+    "Youtube",
+    "Youtube",
+    "Youtube",
+    "Youtube",
+    "Youtube",
+    "Youtube",
 ];
 
 var lengths = [
@@ -52,7 +58,8 @@ function displaySongInfo(){
     });
     
     images_links.forEach(function(image){
-        $("#images").append("<p>" + image + "</p>");
+        var textElement = `<img src = ${image}>`;
+        $("#images").append(textElement);
     });
     
     artists.forEach(function(artist){
@@ -62,6 +69,10 @@ function displaySongInfo(){
     lengths.forEach(function(length){
        $("#lengths").append("<p>" + length + "</p>"); 
     });
+    
+    links.forEach(function(link){
+       $("#links").append("<a>" + link + "</a>");
+    });
     // BELOW Use forEach Loop to display the data from each of your array's in the correct div
 
 }
@@ -69,18 +80,32 @@ function displaySongInfo(){
 function emptySongInfo(){
     $("#songs").empty();
     // Use jQuery to empty all of the remaining divs
-
-
+    $("#images").empty();
+    $("#artists").empty();
+    $("#lengths").empty();
+    $("#links").empty();
 }
-
 
 function addSongInfo(){
     var songName = $("#song").val();
     songs.push(songName);
     // BELOW write the code to add new items to each of the other arrays
-
-
+    var imageName = $("#image").val();
+    images_links.push(imageName);
+    console.log(images_links);
+    var artistName = $("#artist").val();
+    artists.push(artistName);
+    
+    var lengthTime = $("#length").val();
+    lengths.push(lengthTime);
+    
+    var linkName = $("#link").val();
+    links.push(linkName);
 }
+
+$("#delete").click(function(){
+    emptySongInfo();
+});
 
 $("#add").click(function() {
     emptySongInfo();
@@ -89,3 +114,4 @@ $("#add").click(function() {
 });
 
 displaySongInfo();
+
